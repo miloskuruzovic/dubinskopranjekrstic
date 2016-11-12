6db967d50458c50be38481fd6e595bf271bfa148
miloskuruzovic <miloskuruzovic@gmail.com>
2016-11-12 09:54:18 +0100

    added banners,made SEO optimization

--- index.php
+++ index.php
@@ -8,17 +8,27 @@
   <meta name="keywords" content="dubinsko pranje ciscenje automobila namestaja fotelje dvosedi trosedi beograd antibakterijsko Lavor Karcher deep cleaning belgrade">
   <meta name="author" content="miloskuruzovic@gmail.com">
   <link rel="alternate" hreflang="sr" href="http://dubinskopranjekrstic.rs/">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="style.css"/>
   <link rel="icon" type="image/png" href="img/icon2.png">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
-  <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular-route.js"></script>  
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular-route.js"></script>  
   <script src="js/angularapp.js"></script>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-87014408-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
-<body>
+<body id="myPage">
 <nav id="navigation" class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -27,14 +37,15 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#/"><img id="logo" src="img/logo.png"></a>
+      <a class="navbar-brand" href="#/"><img id="logo" src="img/logo.png" alt="logo">
+      <span style="display: none;">Home page</span></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <li><a href="#/">Home</a></li>
         <li><a href="#/cenovnik">Cenovnik</a></li>
-        <li><a href="#/contact">Kontakt</a></li>
         <li><a href="#/apr">APR</a></li>
+        <li><a href="#/contact">Kontakt</a></li>
       </ul>
     </div>
   </div>
@@ -43,7 +54,7 @@
       <h1>Dubinsko pranje Krstić</h1>
       <h5>Najpovoljnije dubinsko pranje automobila, namestaja i tepiha u Beogradu uz pomoć najnovijih Lavor i Karcher mašina. Dolazimo na kućnu adresu!</h5>
     </div>
-<div id="main" ng-app="DubinskoApp" ng-view>
+<div id="main" data-ng-app="DubinskoApp" data-ng-view>
 </div>
 <footer class="container-fluid text-center">
   <div class="row" id="footer_main">
@@ -53,8 +64,8 @@
       <ul>
         <li><a href="#/">Home</a></li>
         <li><a href="#/cenovnik">Cenovnik</a></li>
-        <li><a href="#/contact">Kontakt</a></li>
         <li><a href="#/apr">APR</a></li>
+        <li><a href="#/contact">Kontakt</a></li>
       </ul>
     </div>
     <div class="col-sm-4" id="footer_social">
@@ -62,10 +73,12 @@
       <p id="social">
         <a href="https://www.facebook.com/DubinskoKrstic/" target="_blank" id="fb_link">
         <i class="fa fa-facebook-square" aria-hidden="true"></i>
+        <span style="display: none;">Facebook page</span>
         </a>
           &nbsp;&nbsp;
         <a href="https://twitter.com/DPranje" target="_blank" id="tw_link">
         <i class="fa fa-twitter-square" aria-hidden="true"></i>
+        <span style="display: none;">Twitter profile</span>
         </a>
       </p>
       <p>I budite u toku sa našim vestima i promocijama!</p>
@@ -76,10 +89,54 @@
       <p><i class="fa fa-mobile" aria-hidden="true"></i> &nbsp; 064 / 021 72 42 </p>
     </div>
   </div>
+  <div class="row" id="btt">
+    <a id="up_link" class="up-arrow" href="#myPage" data-toggle="tooltip" title="Povratak na vrh stranice">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </a>
+  </div>
   <div class="row" id="copyright">
-    Developed by : <a href="mailto:miloskuruzovic@gmail.com" target="_blank">Miloš Kuruzović</a>
+    &copy; 2016
+    Developed by :  
+    <a data-toggle="modal" data-target="#myModal">Miloš Kuruzović</a> 
+    <i class="fa fa-hand-o-left" aria-hidden="true"></i><br>
+    <!--
+    Contact me at : <span class="highlight" title="Izrada i održavanje web sajtova">miloskuruzovic@gmail.com</span>
+    -->
+  </div>
+    <div id="myModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h3 class="modal-title">Miloš Kuruzović</h3>
+            <h4>Izrada i održavanje web sajtova</h4>
+          </div>
+          <div class="modal-body">
+            <h4>Izrada unikatnih i modernih web prezentacija, prilagodljivih svim vrstama uređaja (Responsive Web Design).</h4>
+            <hr>
+            <p class="text-left">
+              Kontakt telefon (WhatsApp, Viber): <br><br>
+              <i class="fa fa-mobile" aria-hidden="true"></i> 
+              <i class="fa fa-whatsapp" aria-hidden="true"></i> 
+              &nbsp;<a href="tel:+381641964112">+381 64 1964112</a>
+            </p>
+            <hr>
+            <p class="text-left">
+              Email: <br><br>
+              <i class="fa fa-envelope-o" aria-hidden="true"></i> &nbsp;
+              <a href="mailto:miloskuruzovic@gmail.com">
+               miloskuruzovic@gmail.com
+              </a>
+            </p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal">Zatvori</button>
+          </div>
+        </div>
+      </div>
   </div>
 </footer>
 <script src="js/slidein.js"></script>
+<script src="js/smooth_scroll.js"></script>
 </body>
 </html>
--- js/slidein.js
+++ js/slidein.js
@@ -3,8 +3,30 @@ $(window).scroll(function() {
     var pos = $(this).offset().top;
 
     var winTop = $(window).scrollTop();
-    if (pos < winTop + 830) {
+    if (pos < winTop + 680) {
       $(this).addClass("slide");
+    }
+  });
+});
+
+$(window).scroll(function() {
+  $(".slideanim_right").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 550) {
+      $(this).addClass("slide_right");
+    }
+  });
+});
+
+$(window).scroll(function() {
+  $(".slideanim_left").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 550) {
+      $(this).addClass("slide_left");
     }
   });
 });
